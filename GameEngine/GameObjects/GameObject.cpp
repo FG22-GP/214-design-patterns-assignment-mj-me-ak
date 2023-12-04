7ebd86b6746623fe368909b2828d6a64a0757289
manijs82 <Manijavdani1@gmail.com>
2023-12-04 14:24:34 +0100

    gameobject implementation done

--- GameEngine/GameObjects/GameObject.cpp
+++ GameEngine/GameObjects/GameObject.cpp
@@ -7,6 +7,7 @@ void GameObject::Init()
 {
     transform = new Transform();
     objectRenderer = new ObjectRenderer();
+    components = {};
 
     transform->Init(transform, objectRenderer);
     objectRenderer->Init(transform, objectRenderer);
@@ -20,13 +21,24 @@ void GameObject::Start()
 
 void GameObject::Update()
 {
-    
+    transform->Update();
+    objectRenderer->Update();
+    for (Component* component : components)
+    {
+        component->Update();
+    }
 }
 
 void GameObject::End()
 {
     transform->End();
     objectRenderer->End();
+
+    const int componentCount = components.size();
+    for (int i = componentCount - 1; i >= 0; --i)
+    {
+        RemoveComponent(components[i]);
+    }
 
     delete transform;
     delete objectRenderer;
--- GameEngine/GameObjects/GameObject.h
+++ GameEngine/GameObjects/GameObject.h
@@ -17,6 +17,52 @@ public:
     Transform* transform;
     ObjectRenderer* objectRenderer;
 
-    void AddComponent(Component* component);
-    void RemoveComponent(Component* component);
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>>* = nullptr>
+    T* AddComponent();
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>>* = nullptr>
+    T* GetComponent();
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>>* = nullptr>
+    void RemoveComponent(T* component);
 };
+
+template <typename T, std::enable_if_t<std::is_base_of_v<Component, T>>*>
+T* GameObject::AddComponent()
+{
+    Component* component = new T();
+    components.push_back(component);
+
+    component->Init(transform, objectRenderer);
+    component->Start();
+    
+    return component;
+}
+
+template <typename T, std::enable_if_t<std::is_base_of_v<Component, T>>*>
+T* GameObject::GetComponent()
+{
+    for (std::vector<Component*>::iterator it = components.begin(); it != components.end(); ++it)
+    {
+        if (typeid(*(*it)) == typeid(T))
+        {
+            return *it;
+        }
+    }
+
+    return nullptr;
+}
+
+template <typename T, std::enable_if_t<std::is_base_of_v<Component, T>>*>
+void GameObject::RemoveComponent(T* component)
+{
+    // remove if it contains
+    for (std::vector<Component*>::iterator it = components.begin(); it != components.end(); ++it)
+    {
+        if (*it == component)
+        {
+            it = components.erase(it);
+            (*it)->End();
+            delete (*it);
+            break;
+        }
+    }
+}
